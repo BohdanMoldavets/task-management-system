6de4cd20f1d76e2a43b36e02de0017c472421951
BohdanMoldavets <143205638+BohdanMoldavets@users.noreply.github.com>
2025-03-19 12:41:11 +0100

    Optimise method getAllEmployees for EmployeeController class

--- src/main/java/com/moldavets/task_management_system/employee/controller/EmployeeController.java
+++ src/main/java/com/moldavets/task_management_system/employee/controller/EmployeeController.java
@@ -6,33 +6,27 @@ import com.moldavets.task_management_system.employee.mapper.EmployeeDtoMapper;
 import com.moldavets.task_management_system.employee.model.Employee;
 import com.moldavets.task_management_system.employee.service.EmployeeService;
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/employees")
+@RequiredArgsConstructor
 public class EmployeeController {
 
     private final EmployeeService employeeService;
 
-    @Autowired
-    public EmployeeController(EmployeeService employeeService) {
-        this.employeeService = employeeService;
-    }
-
     @GetMapping
     public ResponseEntity<List<ResponseEmployeeDto>> getAllEmployees() {
-        List<ResponseEmployeeDto> responseEmployeeList = new ArrayList<>();
-        employeeService.getAllEmployees().forEach(employee -> responseEmployeeList.add(
-                        EmployeeDtoMapper.mapToResponseEmployeeDto(employee)
-                ));
-        return new ResponseEntity<>(responseEmployeeList, HttpStatus.OK);
+        return new ResponseEntity<>(employeeService.getAllEmployees().stream()
+                .map(EmployeeDtoMapper::mapToResponseEmployeeDto)
+                .toList(),
+                HttpStatus.OK);
     }
 
     @GetMapping("/{employeeId}")
@@ -64,6 +58,6 @@ public class EmployeeController {
     @DeleteMapping("/{employeeId}")
     public ResponseEntity<HttpStatusCode> deleteEmployeeById(@PathVariable("employeeId") Long employeeId) {
         employeeService.deleteById(employeeId);
-        return ResponseEntity.status(HttpStatus.OK).build();
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
